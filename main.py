f03fb0e050b51213a953785c4ae2396e6ecfef21
khanghuynh2 <hnakhang260504@gmail.com>
2026-03-09 15:55:16 +0700

    lololo

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 
 # Nhập số n từ bàn phím
 n = int(input("Nhập số n: "))
+
 tong = 0
+
 # Tính tổng từ 1 đến n
 for i in range(1, n + 1):
     tong += i
+
 # In kết quả
 print("Tổng các số từ 1 đến", n, "là:", tong)
